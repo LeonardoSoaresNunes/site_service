68fbc07582dff6c9e3ab2bfad907a33edf052bd6
Leonardo Nunes <47831186+LeonardoSoaresNunes@users.noreply.github.com>
2022-02-25 08:59:32 -0300

    Projeto Laravel and MySql

---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+
+ Aula 7    talvez voltar a aula 3 e 2  minuto y
+
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ]);
