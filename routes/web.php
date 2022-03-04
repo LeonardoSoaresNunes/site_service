99749fbd77681f958fd29eb2775a1ed93ffa4dfc
Leonardo Nunes <47831186+LeonardoSoaresNunes@users.noreply.github.com>
2022-03-04 09:52:21 -0300

    Instalando dependencias

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,12 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::any('/posts/search', [PostController::class,'search'])->name('post.search');
 Route::get('/posts/create', [PostController::class,'create'])->name('post.create');
 Route::put('/posts/{id}', [PostController::class,'update'])->name('post.update');
 Route::get('/posts/edit{id}', [PostController::class,'edit'])->name('post.edit');
-Route::delete('/posts/search', [PostController::class,'destroy'])->name('post.destroy');
-Route::get('/posts/search', [PostController::class,'show'])->name('post.show');
+Route::delete('/posts/{id}', [PostController::class,'destroy'])->name('post.destroy');
+Route::get('/posts/{id}', [PostController::class,'show'])->name('post.show');
 Route::post('/posts', [PostController::class,'store'])->name('post.store');
 Route::get('/posts', [PostController::class,'index'])->name('post.index');
 
